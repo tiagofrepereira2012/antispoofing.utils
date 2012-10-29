83194582732082810782d7888f82a2bc92cce98e
Tiago Freitas Pereira <tiagofrepereira@gmail.com>
2012-10-29 18:35:28 +0100

    Add a function that calculate the performance results given a predefined threshold

--- antispoofing/utils/ml/perf.py
+++ antispoofing/utils/ml/perf.py
@@ -103,3 +103,40 @@ def perf_hter(test_scores, devel_scores, threshold_func):
   devel_text = " d: FAR %.2f%% / FRR %.2f%% / HTER %.2f%% " % (100*devel_far, 100*devel_frr, devel_hter)
   test_text = " t: FAR %.2f%% / FRR %.2f%% / HTER %.2f%% " % (100*test_far, 100*test_frr, test_hter)
   return (test_hter, devel_hter), (test_text, devel_text), thres
+
+
+
+def perf_hter_threshold(test_scores, devel_scores, thres):
+  """Computes a performance table and returns the HTER for the test and
+  development set, as well as a formatted text with the results and the value
+  of the threshold obtained for the given threshold value
+
+  Keyword parameters:
+
+    test_scores
+      the scores of the samples in the test set
+    devel_scores
+      the scores of the samples in the development set
+    thres
+      the value of threshold
+  """ 
+   
+  from bob.measure import farfrr
+
+  devel_attack_scores = devel_scores[1]
+  devel_real_scores = devel_scores[0]
+  test_attack_scores = test_scores[1]
+  test_real_scores = test_scores[0]
+
+  devel_real = devel_real_scores.shape[0]
+  devel_attack = devel_attack_scores.shape[0]
+  test_real = test_real_scores.shape[0]
+  test_attack = test_attack_scores.shape[0]
+
+  devel_far, devel_frr = farfrr(devel_attack_scores, devel_real_scores, thres)
+  test_far, test_frr = farfrr(test_attack_scores, test_real_scores, thres)
+  devel_hter = 50 * (devel_far + devel_frr)
+  test_hter = 50 * (test_far + test_frr)
+  devel_text = " d: FAR %.2f%% / FRR %.2f%% / HTER %.2f%% " % (100*devel_far, 100*devel_frr, devel_hter)
+  test_text = " t: FAR %.2f%% / FRR %.2f%% / HTER %.2f%% " % (100*test_far, 100*test_frr, test_hter)
+  return (test_hter, devel_hter), (test_text, devel_text)
