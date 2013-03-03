db38e9a63958695d472153552581ae4d4443215d
Tiago Freitas Pereira <tiagofrepereira@gmail.com>
2013-03-03 11:44:56 -0300

    Prepared the output for the ICB 2013 competition

--- antispoofing/utils/helpers/__init__.py
+++ antispoofing/utils/helpers/__init__.py
@@ -1,1 +1,40 @@
 from .score_reader import *
+
+def write_icb2013_score(scores_list, output_dir):
+  """
+  Write the files for the ICB 2013 competition
+
+  Keyword parameters:
+
+    scores_list
+      List of scores. The format of the list is
+
+      [
+        [file_name, 1.0],
+        [file_name, -1.0]
+        .
+        .
+        .
+        [file_name, 0.0]
+      ]
+
+   output_dir
+     Output directory
+  """
+  import os
+  
+  def write_txt(path,list):
+    f     = open(path,'w')
+    for l in list:
+      f.write(l[0] + " " + str(l[1]) + "\n")
+    f.close()
+	
+  train_list     = [s for s in scores_list if(s[0][0:5]=='train')]
+  devel_list     = [s for s in scores_list if(s[0][0:5]=='devel')]
+  anonymous_list = [s for s in scores_list if(s[0][0:5]=='anony')]
+
+  write_txt(os.path.join(output_dir,'train.txt'),train_list)
+  write_txt(os.path.join(output_dir,'devel.txt'),devel_list)
+  write_txt(os.path.join(output_dir,'anonymous.txt'),anonymous_list)
+
+  
