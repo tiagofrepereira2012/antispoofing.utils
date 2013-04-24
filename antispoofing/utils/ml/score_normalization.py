b61e491de53398f3419b9763acb964a108fd0436
Ivana Chingovska <ivana.chingovska@idiap.ch>
2013-04-24 15:42:53 +0200

    added methods for manually setting norm parameters and for inverse z-norm

--- antispoofing/utils/ml/score_normalization.py
+++ antispoofing/utils/ml/score_normalization.py
@@ -11,31 +11,41 @@ class ScoreNormalization:
   Class that implements some score normalization algorithms
   """
 
-  def __init__(self,scores):
+  def __init__(self, scores=None):
     """
     " Constructor
-    " @param scores set of scores to help in the normaliztion
+    " @param scores set of scores to help in the normalization
     """
+
+    if scores!= None:
+      self.mins = numpy.min(scores,axis=0)
+      self.maxs = numpy.max(scores,axis=0)
     
-    self.mins = numpy.min(scores,axis=0)
-    self.maxs = numpy.max(scores,axis=0)
-    
-    self.avg =  numpy.average(scores,axis=0)
-    self.std  = numpy.std(scores,axis=0)
+      self.avg =  numpy.average(scores,axis=0)
+      self.std  = numpy.std(scores,axis=0)
 
-    self.totalScores = len(scores)
+      self.totalScores = len(scores)
+        
 
-
-  def __str__(self):
-    string =  "%d Scores \n min=%.2f \n max=%.2f \n Average=%.2f \n Standard Deviation=%.2f " % \
+  def __str__(self): 
+    string = ''
+    if hasattr(self, 'totalScores'):      
+      string =  "%d Scores \n" + "min = " + str(self.mins) + "\nmax = " + str(self.maxs) + "\navg = " + str(self.avg) + "\nstd = " + str(self.std)
+      '''string =  "%d Scores \n min=%.2f \n max=%.2f \n Average=%.2f \n Standard Deviation=%.2f " % \
               (self.totalScores, 
               self.mins, 
               self.maxs, 
               self.avg, 
               self.std,
-              )
-    
+             )'''
     return string
+
+  def set_norm_params(self, mins, maxs, avg, std):
+    self.mins = mins
+    self.maxs = maxs
+    self.avg = avg
+    self.std = std
+    self.totalScores = 0
 
 
   def calculateZNorm(self,scores):
@@ -47,6 +57,17 @@ class ScoreNormalization:
     """
 
     return numpy.divide((scores - self.avg),self.std)
+
+  def inverseZNorm(self, normscores, dim):
+    """
+    De-normalize the input cores with ZNorm score normalization
+ 
+    @param scores numpy.array to be de-normalized
+    @dim tuple of the first and last dimension to be considered 
+    @return numpy.array with de-normalized scores
+    """
+    begindim = dim[0]; enddim = dim[1] + 1
+    return self.std[begindim:enddim] * normscores[:,begindim:enddim] + self.avg[begindim:enddim]
 
 
   def calculateMinMaxNorm(self,scores,lowerBound = -1, upperBound = 1):
