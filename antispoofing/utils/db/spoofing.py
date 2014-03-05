2965486d3bd2201f1f6892632d2813974318569d
Ivana Chingovska <ivana.chingovska@idiap.ch>
2014-03-05 17:10:45 +0100

    support for querying CASIA_FASD by folds

--- antispoofing/utils/db/spoofing.py
+++ antispoofing/utils/db/spoofing.py
@@ -43,6 +43,9 @@ class DatabaseAll(DatabaseBase):
 
   def name(self):
     return 'Fused datasets (all)'
+    
+  def short_name(self):
+    return 'all'  
 
   def version(self):
     import pkg_resources  # part of setuptools
@@ -80,40 +83,55 @@ class DatabaseAll(DatabaseBase):
     # does not implement the given access protocol
     return False
 
+  def set_foldsdir(self, foldsdir):    
+    """Sets the directory holding the cross validation protocol for the CASIA_FASD"""
+    self.foldsdir = foldsdir
 
-  def get_train_data(self):
+  def get_train_data(self, fold_no=1):
     import pkg_resources
     real_data = []; attack_data = [];
     for entrypoint in pkg_resources.iter_entry_points('antispoofing.utils.db'):
       plugin = entrypoint.load()
       db = plugin()
-      r, a = db.get_train_data()
+      if db.short_name() == 'casia_fasd':
+        db.set_foldsdir(self.foldsdir)
+        r, a = db.get_train_data(fold_no=fold_no)
+      else:
+        r, a = db.get_train_data()
       real_data = real_data + r
       attack_data = attack_data + a
 
     return (real_data,attack_data)
   get_train_data.__doc__ = DatabaseBase.get_train_data.__doc__
 
-  def get_devel_data(self):
+  def get_devel_data(self, fold_no=1):
     import pkg_resources
     real_data = []; attack_data = [];
     for entrypoint in pkg_resources.iter_entry_points('antispoofing.utils.db'):
       plugin = entrypoint.load()
       db = plugin()
-      r, a = db.get_devel_data()
+      if db.short_name() == 'casia_fasd':
+        db.set_foldsdir(self.foldsdir)
+        r, a = db.get_devel_data(fold_no=fold_no)
+      else:
+        r, a = db.get_devel_data()
       real_data = real_data + r
       attack_data = attack_data + a
 
     return (real_data,attack_data)
   get_devel_data.__doc__ = DatabaseBase.get_devel_data.__doc__
 
-  def get_test_data(self):
+  def get_test_data(self, fold_no=1):
     import pkg_resources
     real_data = []; attack_data = [];
     for entrypoint in pkg_resources.iter_entry_points('antispoofing.utils.db'):
       plugin = entrypoint.load()
       db = plugin()
-      r, a = db.get_test_data()
+      if db.short_name() == 'casia_fasd':
+        db.set_foldsdir(self.foldsdir)
+        r, a = db.get_test_data(fold_no=fold_no)
+      else:
+        r, a = db.get_test_data()
       real_data = real_data + r
       attack_data = attack_data + a
 
@@ -133,8 +151,23 @@ class DatabaseAll(DatabaseBase):
     return (real_data,attack_data)
   get_all_data.__doc__ = DatabaseBase.get_all_data.__doc__
 
+  def get_clients(self, group):
+    raise NotImplementedError("You cannot query for client ids of this database because it is just a dumb fusion of all databases available in your current environment")
+
+  def get_enroll_data(self, group):
+    raise NotImplementedError("You cannot query for enrollment data of this database because it is just a dumb fusion of all databases available in your current environment")
+
   def get_test_filters(self):
     raise NotImplementedError("You cannot query for filters to the test set of this database because it is just a dumb fusion of all databases available in your current environment")
 
   def get_filtered_test_data(self, filter):
     raise NotImplementedError("You cannot apply a filter to the test set of this database because it is just a dumb fusion of all databases available in your current environment")
+    
+  def get_filtered_devel_data(self, filter):
+    raise NotImplementedError("You cannot apply a filter to the devel set of this database because it is just a dumb fusion of all databases available in your current environment")  
+    
+  def get_protocols(self):
+    raise NotImplementedError("You cannot query for protocols of this database because it is just a dumb fusion of all databases available in your current environment")    
+    
+  def get_attack_types(self):
+    raise NotImplementedError("You cannot query for attack types of this database because it is just a dumb fusion of all databases available in your current environment")    
