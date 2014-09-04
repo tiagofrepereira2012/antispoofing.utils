2a1e65436d8f37d71a7c7f5c3b89a3a71fc6db33
Ivana Chingovska <ivana.chingovska@idiap.ch>
2014-09-04 17:51:13 +0200

    updating .travis

--- antispoofing/utils/__init__.py
+++ antispoofing/utils/__init__.py
@@ -3,3 +3,22 @@ from . import faceloc
 from . import helpers
 from . import ml
 
+def get_config():
+  """Returns a string containing the configuration information.
+  """
+
+  import pkg_resources
+
+  packages = pkg_resources.require(__name__)
+  this = packages[0]
+  deps = packages[1:]
+
+  retval =  "%s: %s (%s)\n" % (this.key, this.version, this.location)
+  retval += "  - python dependencies:\n"
+  for d in deps: retval += "    - %s: %s (%s)\n" % (d.key, d.version, d.location)
+
+  return retval.strip()
+
+# gets sphinx autodoc done right - don't remove it
+__all__ = [_ for _ in dir() if not _.startswith('_')]
+
