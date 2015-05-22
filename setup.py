0de88ce43c22462b92036e9eef7f8b713786394b
Manuel Guenther <manuel.guenther@idiap.ch>
2015-05-22 17:18:45 +0200

    Put travis builds on wheels

--- setup.py
+++ setup.py
@@ -16,7 +16,11 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-from setuptools import setup, find_packages
+from setuptools import setup, find_packages, dist
+dist.Distribution(dict(setup_requires=['bob.extension']))
+
+from bob.extension.utils import load_requirements
+install_requires = load_requirements()
 
 # Define package version
 version = open("version.txt").read().rstrip()
@@ -40,15 +44,7 @@ setup(
     include_package_data = True,
     zip_safe = False,
 
-    install_requires=[
-      "setuptools",
-      "six",
-      "bob.ip.color",
-      "bob.ip.draw",
-      "bob.learn.linear",
-      "bob.measure",
-      "bob.learn.libsvm"
-    ],
+    install_requires = install_requires,
 
     namespace_packages = [
       'antispoofing',
