85232152db0cef6f83de3044d5e0598f9039ed9f
Guillaume HEUSCH <guillaume.heusch@idiap.ch>
2015-08-19 08:55:45 +0200

    add face crop for MSU-MFSD database

--- antispoofing/utils/faceloc/__init__.py
+++ antispoofing/utils/faceloc/__init__.py
@@ -50,6 +50,18 @@ def expand_detections(detections, nframes, max_age=-1, faceSizeFilter=0):
 
 def read_face(filename):
   """Reads a single file containing the face locations.
+  
+  Originally developed for the REPLAY-ATTACK database, where each line of the facefile contains:
+    - the frame number
+    - the first two numbers are the x,y coordinates of the top-left corner
+    - the width of the bounding box
+    - the height of the bounding box
+
+  Meant to work also with the MSU-MSFD database, where the facefile provides the following information:
+    - each line contains 9 numbers
+    - the first one is the number of the frame
+    - the 4 following numbers are the coordinates of the face bounding box: left (x), top (y), right(x), bottom (y)
+    - the 4 last numbers are the coordinates of the eyes.
 
   Parameters:
 
@@ -79,11 +91,56 @@ def read_face(filename):
     if len(s) < 4:
       raise RuntimeError("Cannot make sense of data in line %d of file '%s': '%s'" % \
           (i, filename, " ".join(line)))
-    retval[int(s[0])] = BoundingBox(s[1], s[2], s[3], s[4])
+    # check if we have a file for the MSU database (lines are longer since they have the eyes coordinates...)
+    if len(s) == 9:
+      retval[int(s[0])] = BoundingBox(s[1], s[2], int(s[3]) - int(s[1]), int(s[4]) - int(s[2]))
+    else:
+      retval[int(s[0])] = BoundingBox(s[1], s[2], s[3], s[4])
 
   return retval
 
-def preprocess_detections(filename, nFrames, facesize_filter=0, max_age=-1):
+def read_face_msu(filename):
+  """
+  Reads a single file containing the face locations.
+
+
+  Parameters:
+
+  filename
+    the name of the text file containing the face locations
+
+  Returns: A dictionary containing the frames in which detection occurred and
+  with keys corresponding to BoundingBox objects:
+
+    * Bounding box top-left X coordinate
+    * Bounding box top-left Y coordinate
+    * Bounding box width
+    * Bounding box height
+
+  """
+  f = open(filename, 'rt') #opens the file for reading
+
+  # we read all lines that are not empty
+  lines = [k.strip() for k in f.readlines() if k.strip()]
+
+  # iteratively transform the data in every line and store it on the
+  # to-be-returned dictionary
+  retval = {}
+  for i, line in enumerate(lines):
+    s = re.split('; | |, ',line) #s = line.split()
+    if len(s) < 4:
+      raise RuntimeError("Cannot make sense of data in line %d of file '%s': '%s'" % \
+          (i, filename, " ".join(line)))
+    x_topleft = s[1]
+    y_topleft = s[2]
+    width = int(s[3]) - int(s[1])
+    height = int(s[4]) - int(s[2])
+    retval[int(s[0])] = BoundingBox(x_topleft, y_topleft, width, height)
+
+  return retval
+
+
+def preprocess_detections(filename, nFrames, facesize_filter=0, max_age=-1, db='replay'):
   """Reads a single face with the face locations getting the best possible
   detections taking into consideration the ages of the last valid detection
 
@@ -102,10 +159,18 @@ def preprocess_detections(filename, nFrames, facesize_filter=0, max_age=-1):
     An integer indicating for a how many frames a detected face is valid if no
     detection occurs after such frame. A value of -1 == forever
 
+  db
+    a string for the database. If the MSU-MFSD database is used, you should
+    set this parameter to 'msu'. This is done because the face file containing
+    the face coordinates are not in the same format for this database. 
+
   Returns dictionary containing the frames in which detection occurred and with
   keys corresponding to BoundingBox objects.
   """
 
+  #if db == 'msu':
+   # locations = read_face_msu(filename)
+  #else:
   locations = read_face(filename)
   locations = expand_detections(locations,nFrames,faceSizeFilter=facesize_filter,max_age=max_age)
 
